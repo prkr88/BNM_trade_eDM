eef4b0818923447b132e819905edd3587edddcd1
Parker <parker_michael@me.com>
2015-03-24 12:35:31 +1100

    init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+#Food for Life eDM
+##Gulp recipe builds eDM using Jade/SASS and moves styles inline.
+
+###Installation
+1. Clone the repo
+2. `$ cd <path>` into the directory
+3. `$ npm install` install dependancies
+4. `$ gulp` run gulp
+
+###Troubleshooting
+
+##### Gulp command not recognised
+Make sure gulp is installed by running `$ npm install gulp -g`
+
+##### Gulp-sass throws an error
+Gulp sass is only supported upto v0.12 of node. run `$ node -v` to check the version installed
---
+++ gulpfile.js
@@ -0,0 +1,117 @@
+var gulp 		= require('gulp');
+var sass 		= require('gulp-sass');
+var notify 		= require('gulp-notify');
+var inlineCss 	= require('gulp-inline-css');
+var jade		= require('gulp-jade');
+var email 		= require('gulp-email');
+var ghPages		= require('gulp-gh-pages');
+var browserSync = require('browser-sync');
+var reload 		= browserSync.reload;
+
+var options = {
+	user: 'api:key-a51cf11f46a793c555e2447cdd681e8f',
+	url: 'https://api.mailgun.net/v3/sandbox954d5f24aa234124a8545deb66a02b4f.mailgun.org/messages',
+	form: {
+		from: 'Gulp Automator <postmaster@sandbox954d5f24aa234124a8545deb66a02b4f.mailgun.org>',
+		replyto: 'Michael Parker <mparker@brandnewmedia.com.au>',
+		to: [
+		'Michael Parker <redharvestredharvest@gmail.com>',
+		// 'Tom Garton <tgarton@brandnewmedia.com.au>',
+		// 'Aron Du-Shane <adushane@brandnewmedia.com.au>'
+		// 'Dawn Jeremiah <DJeremiah@brandnewmedia.asia>',
+		// 'Claudia Holmes <cholmes@brandnewmedia.com.au>',
+		// 'Nick Fawbert <NickF@brandnewmedia.asia>',
+		// 'Emill Marlinda <EMarlinda@brandnewmedia.asia>'
+		],
+		cc:[
+			'Michael Parker <mparker@brandnewmedia.com.au>',
+			// 'Tom Garton <TGarton@brandnewmedia.com.au>',
+			// 'Natalie Johnson <NJohnson@brandnewmedia.com.au>',
+			// 'Magdalena Trajkoska <mtrajkoska@brandnewmedia.com.au>'
+		],
+		subject: 'BNM Trade eDM'
+	}
+};
+	
+//test the eDM
+gulp.task('send', function(){
+	return gulp.src('./public/index.html')
+		.pipe(email(options));
+});
+
+gulp.task('move', function(){
+	return gulp.src('./build/img/*')
+	.pipe(gulp.dest('./public/assets/'))
+})
+
+//compile jade, catch errors before compile. 
+gulp.task('jade',['move'], function () {
+	var j = jade({
+		pretty: true
+	});
+	j.on('error', function(err){
+		console.log(err);
+		notify().write("jade error");
+		j.end();
+		gulp.watch();
+	})
+	return gulp.src('./build/jade/*.jade')
+	.pipe(j)
+	.pipe(gulp.dest('./build/temp/'))
+});
+
+//compile scss to css
+gulp.task('build', ['jade'], function(){
+	return gulp.src('./build/scss/*.scss')
+		.pipe(sass({
+			style: 'compressed',
+			errLogToConsole: false,
+			onError: function(err){
+				return notify().write(err);
+			}
+		}))
+		.pipe(gulp.dest('./build/temp/css/'));
+});
+
+//move css inline
+gulp.task('inline', ['build'], function(){
+	return gulp.src('./build/temp/index.html')
+		.pipe(inlineCss({
+			removeStyleTags: false
+		}))
+		.pipe(gulp.dest('./public/'))
+		.pipe(reload({stream: true}));
+});
+
+
+gulp.task('compile', ['inline'], function(){
+	return gulp.src('./build/fonts/*')
+		.pipe(gulp.dest('./public/assets/'))
+		.pipe(reload({stream: true}));
+
+})
+
+//compile on change
+gulp.task('watch', function(){
+	gulp.watch(['./build/scss/*.scss', './build/jade/*.jade'], ['compile']);
+});
+
+//serve to the browser
+gulp.task('serve', function(){
+	browserSync({
+		server: {
+			baseDir: "./public"
+		},
+		open: false
+	})
+});
+
+//the dafault task
+gulp.task('default', ['watch', 'serve', 'compile']);
+
+//deploy to github-pages
+gulp.task('deploy', function(){
+	return gulp.src('./public/**/*')
+	.pipe(ghPages());
+});
+
